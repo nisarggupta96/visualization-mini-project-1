dfe81bcc2d4f524d7f99d78e3e31746f8c4255d6
Nisarg <nisarggupta96@gmail.com>
2023-02-15 21:55:26 -0500

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider>
 			<Component {...pageProps} />
+			<Analytics />
 		</ChakraProvider>
 	)
 }
